de92f97d0316e5e937509d43b094589f8a92c0e8
John Hartert <j.blainehartert@gmail.com>
2017-08-10 23:28:27 -0400

    Adds CS work

---
+++ elective_prac.py
@@ -0,0 +1,84 @@
+# Insertion sort
+# array=[10,4,6,8,3,8,8,3,12,5]
+
+# i=1
+# while(i<10):
+#   x=array[i]
+#   j=i
+#   i=i+1
+
+#   while(j>0 and array[j-1]>x):
+#     array[j]=array[j-1]
+#     j=j-1
+
+#   array[j]=x
+
+# i=0
+# while(i<10):
+#   print (array[i])
+#   i=i+1
+
+
+
+
+#   """
+# Print 0 to 3 in a for loop
+# """
+# for i in range(4):
+#     print(i)
+ 
+# """
+# Print 0 to 3 using recursion
+# """
+# def cnt(i):
+#     print(i)
+#     if i < 4:
+#         cnt(i + 1)
+ 
+# cnt(0)
+
+# def example_one(number):
+#         if number == 10:
+#             return
+#         print(number)
+#         example_one(number + 1)
+
+# example_one(0)
+
+# count down from n to 0.
+
+# def fib(x, y, max, counter = 0):
+#     if counter == max:
+#         return
+#     counter += 1
+#     nex_number = x + y
+#     print(x)
+#     fib(y, nex_number, max, counter)
+# fib(x, y, max)
+
+# def count_down(n):
+#     if n == 0:
+#         return
+#     print (n)
+#     count_down(n - 1)
+# count_down(10)
+
+# def fancy_pattern(n, counter):
+#     if n == counter:
+#         return
+#     counter += 1
+
+#     fancy_pattern(n, counter)
+
+
+def factorial(number):
+    if number ==
+        return
+    
+
+
+
+
+
+
+
